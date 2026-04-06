6d0e39b384b10433fbf84f66e8f29c5ef20b440a
Adam Kasper <adam@obvio.cz>
2026-04-06 21:18:09 +0200

    test: update tests for v2 architecture

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,12 @@
-import MyModule from '../../../src/module'
-
 export default defineNuxtConfig({
-  modules: [
-    MyModule,
-  ],
+  modules: ['../../../src/module'],
+
+  unleash: {
+    url: 'http://localhost:3737/api/frontend',
+    token: 'test-token',
+    appName: 'test-app',
+    storage: 'memory',
+    refreshInterval: 60_000,
+    clientRefreshInterval: 0,
+  },
 })
